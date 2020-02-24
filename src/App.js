7fa2efe9a7d7dc0c0ccad4a9dbc821c65cfd0703
Sylvie <sylvie@passions-app.com>
2020-02-24 14:25:53 +0100

    Passing Method reference between Component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, { useState } from 'react';//useState more important react hook
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-//normal function component
-const App = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Sylvie', age: 25 },
       { name: 'Xavier', age: 26 },
       { name: 'Yannick', age: 36 }
-    ]
-  });
+    ],
+    otherState: 'some other valuer'
+  };
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState);
-  const switchNameHandler = () => {
-    // Don't do this: personsState.persons[0].name = 'Naomie';
-    setPersonsState({
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
-        { name: 'Naomie', age: 25 },
+        { name: newName, age: 25 },
         { name: 'Xavier', age: 26 },
-        { name: 'Yannick', age: 37 }
+        { name: 'newName', age: 37 }
       ]
     });
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, i'm a React App</h1>
-      <p>This is really working !</p>
-      <button onClick={switchNameHandler}>Click here</button>
-      <Person name={personsState.persons[0].name}
-              age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name}
-              age={personsState.persons[1].age}/>
-      <Person name={personsState.persons[2].name}
-              age={personsState.persons[2].age}/>
-    </div>
-  );
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Sylvie', age: 25 },
+        { name: event.target.value, age: 26 },
+        { name: 'Yannick', age: 37 }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, i'm a React App</h1>
+        <p>This is really working !</p>
+        <button onClick={() => this.switchNameHandler('Sylvie !!')}>Click here
+        </button>
+        <Person name={this.state.persons[0].name}
+                age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Sylvie')}
+                changed={this.nameChangedHandler}/>
+        <Person name={this.state.persons[2].name}
+                age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
