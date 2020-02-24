bcb4fa09985575d2bdee61e4f4b205f95ed5fd52
Sylvie <sylvie@passions-app.com>
2020-02-24 14:07:53 +0100

    use useState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';//useState more important react hook
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+//normal function component
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Sylvie', age: 25 },
       { name: 'Xavier', age: 26 },
       { name: 'Yannick', age: 36 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    console.log('bien joué');
-  }
+  const [otherState, setOtherState] = useState('some other value');
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, i'm a React App</h1>
-        <p>This is really working !</p>
-        <button onClick={this.switchNameHandler}>Click here</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    )
-  }
+  console.log(personsState);
+  const switchNameHandler = () => {
+    // Don't do this: personsState.persons[0].name = 'Naomie';
+    setPersonsState({
+      persons: [
+        { name: 'Naomie', age: 25 },
+        { name: 'Xavier', age: 26 },
+        { name: 'Yannick', age: 37 }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, i'm a React App</h1>
+      <p>This is really working !</p>
+      <button onClick={switchNameHandler}>Click here</button>
+      <Person name={personsState.persons[0].name}
+              age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name}
+              age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name}
+              age={personsState.persons[2].age}/>
+    </div>
+  );
 }
 
 export default App;
